e95cb154ada63b0b18a739c48336a6f30433a5c5
Julia <julia.ernst@wheregroup.com>
2024-07-30 17:25:57 +0200

    some changes to test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: "My Site",
-  tagline: "Dinosaurs are cool",
+  title: "My Test Site",
+  tagline: "pipline",
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
