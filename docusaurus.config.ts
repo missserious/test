13d6509834acb614bb1226ebdc3794cebaad9da9
Julia <julia.ernst@wheregroup.com>
2024-07-31 09:48:29 +0200

    changes to test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: "My Test-Site",
+  title: "Hello",
   tagline: "pipline",
   favicon: "img/favicon.ico",
 
